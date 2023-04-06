cd2cb521ac1f0e98fa943fae09a2cc788e75314b
hambuchmark <120275202+hambuchmark@users.noreply.github.com>
2023-04-06 12:48:54 +0200

    Create TestApp.java

---
+++ src/test/java/app/TestApp.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    
+    @Test
+    public void testIsPrime() {
+        assertFalse(App.isPrime(1));
+        assertTrue(App.isPrime(2));
+        assertTrue(App.isPrime(3));
+        assertFalse(App.isPrime(4));
+        assertTrue(App.isPrime(5));
+        assertFalse(App.isPrime(6));
+        assertTrue(App.isPrime(7));
+        assertFalse(App.isPrime(8));
+        assertFalse(App.isPrime(9));
+        assertFalse(App.isPrime(10));
+        assertTrue(App.isPrime(11));
+    }
+}
