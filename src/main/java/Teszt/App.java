9f45fbe1bb9e24dbf7f1466f7114fd391815528f
hambuchmark <120275202+hambuchmark@users.noreply.github.com>
2023-04-06 11:20:35 +0200

    Update App.java

--- src/main/java/Teszt/App.java
+++ src/main/java/Teszt/App.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;  
 
-public class PrimeExample4 {  
+public class App {  
     
    public static void main(String[] args) {  
        System.out.println("Hello, World!\n");
